a008b0c83dded576a57914c97f255d544619f177
Karl Koch <karl@kejk.tech>
2022-11-12 22:43:54 +0000

    chore: font swap

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        serif: ["Bitter", ...defaultTheme.fontFamily.serif],
+        serif: ["Bree", ...defaultTheme.fontFamily.serif],
       },
     },
   },
